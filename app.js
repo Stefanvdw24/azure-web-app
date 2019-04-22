cfe77f5719f3f51ccd0296db835107c1570584c7
Daniel <stefanvdw24@gmail.com>
2019-04-22 18:40:59 +0200

    test git for new files

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const port=process.env.PORT || 3000
 
+const routes = require('./routes');
+
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Hello World. some extra text</h1>');
+	res.end('<h1>Hello World. some extra text. 1</h1>');
 });
 
 server.listen(port,() => {
